9febace3ff87a1cff8d9bd07fb6e005e2e25425d
packer-jp <tone0805develop@gmail.com>
2020-05-17 18:41:21 +0900

    update

--- test/unit-test/string/run-length-encoding.test.cpp
+++ test/unit-test/string/run-length-encoding.test.cpp
@@ -1,5 +1,5 @@
 #define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_1_A&lang=ja"
-#include"../../../utility/run-length-encoding.hpp"
+#include"../../../string/run-length-encoding.hpp"
 
 #include<bits/stdc++.h>
 using namespace std;
