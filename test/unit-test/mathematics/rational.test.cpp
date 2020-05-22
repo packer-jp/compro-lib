37729394bb718a8cd274975db66f9fc9f83775f7
packer-jp <tone0805develop@gmail.com>
2020-05-23 02:49:05 +0900

    update

--- test/unit-test/mathematics/rational.test.cpp
+++ test/unit-test/mathematics/rational.test.cpp
@@ -12,7 +12,7 @@ int main() {
     assert(rational(1, 2) - rational(1, 3) == rational(1, 6));
     assert(rational(100, 200) * rational(400, 300) == rational(2, 3));
     assert(rational(100, 200) / rational(300, 400) == rational(2, 3));
-    assert(rational(3, 4) > rational(200, 300));
+    assert(rational(3, 4) > rational(20, 30));
     assert(rational(200, 300) < rational(3, 4));
     assert(rational(2, 3).pow(4) == rational(16, 81));
     assert(rational(-1, 1).pow(3) == rational(-1, 1));
