cd276b358b7aec35f7ac00a3b3c4c11321c7cce6
packer-jp <tone0805develop@gmail.com>
2020-05-23 02:35:07 +0900

    update

--- test/unit-test/mathematics/rational.test.cpp
+++ test/unit-test/mathematics/rational.test.cpp
@@ -17,7 +17,7 @@ int main() {
     assert(rational(2, 3).pow(4) == rational(16, 81));
     assert(rational(-1, 1).pow(3) == rational(-1, 1));
     assert(rational(2, 1).pow(30) < rational(3, 1).pow(20));
-    assert(rational(1, 4).to_double() == 0.25);
+    assert(rational(1, 8).to_double() == 0.125);
 
     cout << "Hello World" << endl;
 }
