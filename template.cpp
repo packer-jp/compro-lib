1722c639573da6f9c02c5531f4a214fbec23869c
packer-jp <39424049+packer-jp@users.noreply.github.com>
2020-06-03 01:48:29 +0900

    update

--- template.cpp
+++ template.cpp
@@ -5,7 +5,7 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (int) (n); i++)
 #define reps(i, n) for (int i = 1; i <= (int) (n); i++)
 #define all(x) (x).begin(), (x).end()
-#define uniq(x) (x).erase(unique(all(x)), (x))
+#define uniq(x) (x).erase(unique(all(x)), (x).end())
 #define bit(n) (1LL << (n))
 #define cdiv(a, b) (((a) - 1) / (b) + 1)
 #define dump(x) cerr << #x " = " << (x) << endl
