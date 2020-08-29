92e32a998a881cf75f70579f74704030d63f3b0f
packer-jp <tone0805develop@gmail.com>
2020-08-30 05:51:33 +0900

    update

--- test/unit-test/mathematics/moebius.test.cpp
+++ test/unit-test/mathematics/moebius.test.cpp
@@ -1,6 +1,7 @@
 #define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_1_A&lang=ja"
 #include"../../../mathematics/moebius.hpp"
 #include"../../../mathematics/prime-factorize.hpp"
+#include"../../../string/run-length-encoding.hpp"
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -9,7 +10,7 @@ int main() {
     int n = 1260;
     map<long long, int> mb = moebius(n);
     for (int i = 1; i <= n; i++) {
-        auto pf = prime_factorize(i);
+        auto pf = run_length_encoding(prime_factorize(i));
         int m = (pf.size() & 1) ? -1 : 1;
         if (n % i) { m = 0; }
         for (int i = 0; i < pf.size(); i++) {
