42ecbf3cb0d2198dc6ffa66def0a443f947fcf18
packer-jp <tone0805develop@gmail.com>
2020-09-01 03:38:06 +0900

    update

--- string/trie.hpp
+++ string/trie.hpp
@@ -10,7 +10,7 @@ struct trie {
     std::vector<std::vector<int>> adj;
     std::vector<int> cnt;
     trie() : adj(1, std::vector<int>(K, -1)), cnt(1) {}
-    void insert(const std::vector<int> &s, bool inv = false) {
+    void insert(const std::vector<int> &s, int m = 1) {
         int cur = 0;
         for (int c:s) {
             if (adj[cur][c] == -1) {
@@ -20,7 +20,7 @@ struct trie {
             }
             cur = adj[cur][c];
         }
-        cnt[cur] += inv ? -1 : 1;
+        cnt[cur] += m;
     }
     std::vector<int> count_prefix(const std::vector<int> &s) {
         std::vector<int> ret(s.size() + 1);
