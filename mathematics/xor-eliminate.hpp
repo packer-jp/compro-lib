6af5fffc73f5e155c8ce357f7851b1c19e27786d
packer-jp <tone0805develop@gmail.com>
2020-09-01 18:40:08 +0900

    update

--- mathematics/xor-eliminate.hpp
+++ mathematics/xor-eliminate.hpp
@@ -8,17 +8,17 @@
 
 template<typename T> std::vector<T> xor_eliminate(std::vector<T> src) {
     std::vector<T> ret;
-    for (int i = src.size() - 1; i >= 0; i--) {
-        for (int j = 0; j < i; j++) {
-            int x = src[j]._Find_first(), y = src[j + 1]._Find_first();
-            if (x < y) { src[j + 1] ^= src[j]; }
-            if (x <= y) { src[j] ^= src[j + 1]; }
+    for (int i = 0; i < src.size(); i++) {
+        for (int j = src.size() - 1; j > i; j--) {
+            int x = src[j]._Find_first(), y = src[j - 1]._Find_first();
+            if (x < y) { src[j - 1] ^= src[j]; }
+            if (x <= y) { src[j] ^= src[j - 1]; }
         }
         if (src[i].none()) { break; }
-        ret.emplace_back(src[i]), src.pop_back();
+        ret.emplace_back(src[i]);
     }
     for (int i = ret.size() - 1; i > 0; i--) {
-        for (int j = i - 1; j >= 0; j--) {
+        for (int j = 0; j < i; j++) {
             if (ret[j][ret[i]._Find_first()]) { ret[j] ^= ret[i]; }
         }
     }
