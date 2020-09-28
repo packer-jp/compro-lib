32cfce029d54e3fec4360d254b2207be54d39d07
packer-jp <tone0805develop@gmail.com>
2020-09-28 21:51:40 +0900

    update

--- template.cpp
+++ template.cpp
@@ -4,30 +4,29 @@ using namespace std;
 #define int long long
 #define rep(i, n) for (int i = 0; i < (int) (n); i++)
 #define reps(i, n) for (int i = 1; i <= (int) (n); i++)
-#define all(x) (x).begin(), (x).end()
-#define uniq(x) (x).erase(unique(all(x)), (x).end())
+#define all(a) (a).begin(), (a).end()
+#define uniq(a) (a).erase(unique(all(a)), (a).end())
 #define bit(n) (1LL << (n))
-#define dump(x) cerr << #x " = " << (x) << endl
+#define dump(a) cerr << #a " = " << (a) << endl
 using vint = vector<int>;
-using vvint = vector<vint>;
 using pint = pair<int, int>;
 using vpint = vector<pint>;
 template<typename T> using priority_queue_rev = priority_queue<T, vector<T>, greater<T>>;
 constexpr double PI = 3.1415926535897932384626433832795028;
 constexpr int DY[9] = {0, 1, 0, -1, 1, 1, -1, -1, 0};
 constexpr int DX[9] = {1, 0, -1, 0, 1, -1, -1, 1, 0};
-int sign(int x) { return (x > 0) - (x < 0); }
+int sign(int a) { return (a > 0) - (a < 0); }
 int gcd(int a, int b) {
     while (b) { swap(a %= b, b); }
     return a;
 }
 int lcm(int a, int b) { return a / gcd(a, b) * b; }
 int cdiv(int a, int b) { return (a - 1 + b) / b; }
-template<typename T> void fin(T mes) {
-    cout << mes << endl;
+template<typename T> void fin(T a) {
+    cout << a << endl;
     exit(0);
 }
-template<typename T> T sq(T x) { return x * x; }
+template<typename T> T sq(T a) { return a * a; }
 template<typename T, typename U> bool chmax(T &a, const U &b) {
     if (a < b) {
         a = b;
@@ -42,46 +41,46 @@ template<typename T, typename U> bool chmin(T &a, const U &b) {
     }
     return false;
 }
-template<typename T, typename U> ostream &operator<<(ostream &os, const pair<T, U> &rhs) {
-    os << "(" << rhs.first << ", " << rhs.second << ")";
+template<typename T, typename U> ostream &operator<<(ostream &os, const pair<T, U> &a) {
+    os << "(" << a.first << ", " << a.second << ")";
     return os;
 }
-template<typename T> ostream &operator<<(ostream &os, const vector<T> &rhs) {
+template<typename T> ostream &operator<<(ostream &os, const vector<T> &a) {
     os << "{";
-    for (auto itr = rhs.begin(); itr != rhs.end(); itr++) {
-        os << *itr << (next(itr) != rhs.end() ? ", " : "");
+    for (auto itr = a.begin(); itr != a.end(); itr++) {
+        os << *itr << (next(itr) != a.end() ? ", " : "");
     }
     os << "}";
     return os;
 }
-template<typename T> ostream &operator<<(ostream &os, const deque<T> &rhs) {
+template<typename T> ostream &operator<<(ostream &os, const deque<T> &a) {
     os << "{";
-    for (auto itr = rhs.begin(); itr != rhs.end(); itr++) {
-        os << *itr << (next(itr) != rhs.end() ? ", " : "");
+    for (auto itr = a.begin(); itr != a.end(); itr++) {
+        os << *itr << (next(itr) != a.end() ? ", " : "");
     }
     os << "}";
     return os;
 }
-template<typename T> ostream &operator<<(ostream &os, const set<T> &rhs) {
+template<typename T> ostream &operator<<(ostream &os, const set<T> &a) {
     os << "{";
-    for (auto itr = rhs.begin(); itr != rhs.end(); itr++) {
-        os << *itr << (next(itr) != rhs.end() ? ", " : "");
+    for (auto itr = a.begin(); itr != a.end(); itr++) {
+        os << *itr << (next(itr) != a.end() ? ", " : "");
     }
     os << "}";
     return os;
 }
-template<typename T> ostream &operator<<(ostream &os, const multiset<T> &rhs) {
+template<typename T> ostream &operator<<(ostream &os, const multiset<T> &a) {
     os << "{";
-    for (auto itr = rhs.begin(); itr != rhs.end(); itr++) {
-        os << *itr << (next(itr) != rhs.end() ? ", " : "");
+    for (auto itr = a.begin(); itr != a.end(); itr++) {
+        os << *itr << (next(itr) != a.end() ? ", " : "");
     }
     os << "}";
     return os;
 }
-template<typename T, typename U> ostream &operator<<(ostream &os, const map<T, U> &rhs) {
+template<typename T, typename U> ostream &operator<<(ostream &os, const map<T, U> &a) {
     os << "{";
-    for (auto itr = rhs.begin(); itr != rhs.end(); itr++) {
-        os << *itr << (next(itr) != rhs.end() ? ", " : "");
+    for (auto itr = a.begin(); itr != a.end(); itr++) {
+        os << *itr << (next(itr) != a.end() ? ", " : "");
     }
     os << "}";
     return os;
