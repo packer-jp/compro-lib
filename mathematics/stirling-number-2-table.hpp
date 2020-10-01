872bc2d7a8c1270ca934f6193a11a3114122f93e
packer-jp <tone0805develop@gmail.com>
2020-10-01 10:19:34 +0900

    update

--- mathematics/stirling-number-2-table.hpp
+++ mathematics/stirling-number-2-table.hpp
@@ -3,6 +3,7 @@
 template<typename T> std::vector<std::vector<T>> stirling_number_2_table(int n, int k) {
     std::vector ret(n + 1, std::vector<T>(k + 1));
     ret[0][0] = 1;
+    if (k == 0) { return ret; }
     for (int i = 1; i <= n; i++) {
         ret[i][1] = 1;
         for (int j = 2; j <= i && j <= k; j++) { ret[i][j] = ret[i - 1][j - 1] + j * ret[i - 1][j]; }
