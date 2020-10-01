4aac168e3c715a2e13853a82a1144efd17871ba6
packer-jp <tone0805develop@gmail.com>
2020-10-01 10:15:45 +0900

    update

--- mathematics/bell-number-table.hpp
+++ mathematics/bell-number-table.hpp
@@ -1,11 +1,10 @@
 #include <vector>
+#include "stirling-number-2-table.hpp"
 
 template<typename T> std::vector<std::vector<T>> bell_number_table(int n, int k) {
-    std::vector ret(n + 1, std::vector<T>(k + 1));
-    ret[0][0] = 1;
-    for (int i = 1; i <= n; i++) {
-        ret[i][1] = 1;
-        for (int j = 2; j <= k; j++) { ret[i][j] = ret[i][j - 1] + ret[i - 1][j - 1] + j * ret[i - 1][j]; }
+    std::vector ret = stirling_number_2_table<T>(n, k);
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j < k; j++) { ret[i][j + 1] += ret[i][j]; }
     }
     return ret;
 }
