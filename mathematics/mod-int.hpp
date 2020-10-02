95da37435ff128a7dc41dee1e0b106f4b3d446eb
packer-jp <tone0805develop@gmail.com>
2020-10-02 21:18:47 +0900

    update

--- mathematics/mod-int.hpp
+++ mathematics/mod-int.hpp
@@ -69,16 +69,17 @@ struct mod_int {
 
 template<typename T>
 struct combination {
-    std::vector<T> fact, fact_inv, inv;
-    combination(int n) : fact(n + 1), fact_inv(n + 1), inv(n + 1) {
-        fact[0] = fact[1] = fact_inv[0] = fact_inv[1] = inv[1] = 1;
+    std::vector<T> fact, finv, inv;
+    combination(int n) : fact(n + 1), finv(n + 1), inv(n + 1) {
+        fact[0] = fact[1] = finv[0] = finv[1] = inv[1] = 1;
         for (int i = 2; i <= n; i++) {
             fact[i] = fact[i - 1] * i;
             inv[i] = -inv[T::mod() % i] * (T::mod() / i);
-            fact_inv[i] = fact_inv[i - 1] * inv[i];
+            finv[i] = finv[i - 1] * inv[i];
         }
     }
-    T P(int n, int r) { return r < 0 || n < r ? 0 : (fact[n] * fact_inv[n - r]); }
-    T C(int n, int r) { return r < 0 || n < r ? 0 : (fact[n] * fact_inv[r] * fact_inv[n - r]); }
+    T P(int n, int r) { return r < 0 || n < r ? 0 : (fact[n] * finv[n - r]); }
+    T C(int n, int r) { return P(n, r) * finv[r]; }
     T H(int n, int r) { return C(n + r - 1, r); }
+    T catalan(int n) { return C(2 * n, n) / (n + 1); }
 };
