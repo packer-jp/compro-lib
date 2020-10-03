3ba54b7c5d5cec83a8aa8fa4a0f58473ba91922e
packer-jp <tone0805develop@gmail.com>
2020-10-04 00:01:02 +0900

    update

--- mathematics/mod-int.hpp
+++ mathematics/mod-int.hpp
@@ -67,6 +67,72 @@ struct mod_int {
     friend std::ostream &operator<<(std::ostream &os, const mod_int &a) { return os << a.v; }
 };
 
+template<int &MOD>
+struct runtime_mod_int {
+    int v;
+    runtime_mod_int(long long v_ = 0) : v(v_ % MOD) { if (v < 0) { v += MOD; }}
+    static int mod() { return MOD; }
+    int val() const { return v; }
+    runtime_mod_int &operator+=(const runtime_mod_int &a) {
+        if ((v += a.v) >= MOD) { v -= MOD; }
+        return *this;
+    }
+    runtime_mod_int &operator-=(const runtime_mod_int &a) {
+        if ((v += MOD - a.v) >= MOD) { v -= MOD; }
+        return *this;
+    }
+    runtime_mod_int &operator*=(const runtime_mod_int &a) {
+        v = (int) ((long long) v * a.v % MOD);
+        return *this;
+    }
+    runtime_mod_int &operator/=(const runtime_mod_int &a) { return *this *= a.inv(); }
+    runtime_mod_int operator+() const { return *this; }
+    runtime_mod_int operator-() const { return -v; }
+    runtime_mod_int operator++() { return *this += 1; }
+    runtime_mod_int operator--() { return *this -= 1; }
+    runtime_mod_int operator++(signed) {
+        const mod_int ret(*this);
+        ++*this;
+        return ret;
+    }
+    runtime_mod_int operator--(signed) {
+        const mod_int ret(*this);
+        --*this;
+        return ret;
+    }
+    runtime_mod_int inv() const {
+        int a = v, b = MOD, x = 1, u = 0;
+        while (b) {
+            int t = a / b;
+            std::swap(a -= t * b, b), std::swap(x -= t * u, u);
+        }
+        return x;
+    }
+    runtime_mod_int pow(long long n) const {
+        if (n < 0) { return pow(-n).inv(); }
+        mod_int ret = 1, mul = *this;
+        while (n) {
+            if (n & 1) { ret *= mul; }
+            mul *= mul, n >>= 1;
+        }
+        return ret;
+    }
+    friend bool operator==(const runtime_mod_int &a, const runtime_mod_int &b) { return a.v == b.v; }
+    friend bool operator!=(const runtime_mod_int &a, const runtime_mod_int &b) {
+        return std::rel_ops::operator!=(a, b);
+    }
+    friend runtime_mod_int operator+(runtime_mod_int a, const runtime_mod_int &b) { return a += b; }
+    friend runtime_mod_int operator-(runtime_mod_int a, const runtime_mod_int &b) { return a -= b; }
+    friend runtime_mod_int operator*(runtime_mod_int a, const runtime_mod_int &b) { return a *= b; }
+    friend runtime_mod_int operator/(runtime_mod_int a, const runtime_mod_int &b) { return a /= b; }
+    friend std::istream &operator>>(std::istream &is, runtime_mod_int &a) {
+        long long v;
+        is >> v, a = v;
+        return is;
+    }
+    friend std::ostream &operator<<(std::ostream &os, const runtime_mod_int &a) { return os << a.v; }
+};
+
 template<typename T>
 struct combination {
     std::vector<T> fact, finv, inv;
