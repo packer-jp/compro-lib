f87114fa871939b24baa48b263c474b341c2a6e5
packer_jp <tone0805develop@gmail.com>
2021-05-11 17:24:41 +0900

    update

---
+++ data-structure/segtree.hpp
@@ -0,0 +1,56 @@
+#include <vector>
+
+template <typename S> struct segment_tree {
+    using T = typename S::T;
+    int n;
+    std::vector<T> data;
+    segment_tree(int n) : n(n), data(n << 1, S::id()) {
+    }
+    segment_tree(const std::vector<T> &src) : n(src.size()), data(n << 1) {
+        std::copy(src.begin(), src.end(), data.begin() + n);
+        for (int i = n - 1; i > 0; i--) {
+            data[i] = S::op(data[i << 1 | 0], data[i << 1 | 1]);
+        }
+    }
+    void set(int i, const T &x) {
+        data[i += n] = x;
+        while (i >>= 1) {
+            data[i] = S::op(data[i << 1 | 0], data[i << 1 | 1]);
+        }
+    }
+    const T &operator[](int i) const {
+        return data[i + n];
+    }
+    T fold(int l, int r) const {
+        T a = S::id(), b = S::id();
+        for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
+            if (l & 1) {
+                a = S::op(a, data[l++]);
+            }
+            if (r & 1) {
+                b = S::op(data[--r], b);
+            }
+        }
+        return S::op(a, b);
+    }
+};
+
+struct rsq {
+    using T = int;
+    static T op(const T &a, const T &b) {
+        return a + b;
+    }
+    static T id() {
+        return 0;
+    }
+};
+
+struct rminq {
+    using T = int;
+    static T op(const T &a, const T &b) {
+        return std::min(a, b);
+    }
+    static T id() {
+        return std::numeric_limits<T>::max();
+    }
+};
